fa996acb0f2819deaff355f838402d325753e41e
idontknow-1 <imera@byupathway.edu>
2026-03-30 20:23:54 -0700

    commit

---
+++ startLabTemple/js/scripts.js
@@ -0,0 +1,39 @@
+import { temples } from "../data/temples.js";
+// console.log(temples);
+
+import { url } from "../data/temples.js";
+// console.log(url);
+
+const showHere = document.querySelector("#showHere");
+const mydialog = document.querySelector('#mydialog');
+const mytitle = document.querySelector('#mydialog h2');
+const myclose = document.querySelector('#mydialog button');
+const myinfo = document.querySelector('#mydialog p');
+// close btn
+myclose.addEventListener("click", () => mydialog.close());
+
+function displayItems(data) {
+    showHere.innerHTML = "";
+    console.log(data);
+    data.forEach( x => {
+        console.log(x)
+        const photo = document.createElement('img');
+        photo.src = `${url}${x.path}`;
+        photo.alt = x.name;
+
+        photo.addEventListener('click', () => showStuff(x));
+        showHere.appendChild(photo);
+    });
+}
+
+displayItems(temples);
+
+function showStuff(x) {
+    mytitle.innerHTML = x.name;
+
+    myinfo.innerHTML = `<strong>Dedicated:</strong> ${x.dedicated} <br>
+        <strong>Person:</strong>${x.person} <br>
+        <strong>Number:</strong>${x.number}`;
+
+    mydialog.showModal();
+}
